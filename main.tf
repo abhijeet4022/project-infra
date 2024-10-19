243e10be31a62c948fb2fd30ed23766a52242b1d
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-19 21:49:57 +0530

    VPC

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 module "vpc" {
   source = "git::https://github.com/abhijeet4022/terraform-aws-vpc.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
-  vpc_name = "${each.key}-vpc"
-
-
+  subnets    = each.value["subnets"]
+  vpc_name   = "${each.key}-vpc"
 }
 
 
-variable "vpc" {}
