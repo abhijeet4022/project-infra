f250b369cbcdceeb49a61bc0d14e64483512caff
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-21 09:00:54 +0530

    ALB

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   main_vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  public_subnets = data.aws_subnets.subnets.ids
+  # public_subnets = data.aws_subnets.subnets.ids
   app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
 }
 
