d4fa3fa84515d6cf452dd9ee6d418df10c82a7af
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-26 14:04:40 +0530

    Project Infra

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@
 #   value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 # }
 
-output "Abhijeet" {
-  value = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
-}
+# output "Abhijeet" {
+#   value = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
+# }
