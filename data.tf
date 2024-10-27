0a31e38b391b750ecdd3bc3bfd04474550eb12bf
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-27 10:36:22 +0530

    Project Infra

--- data.tf
+++ data.tf
@@ -4,3 +4,11 @@ data "aws_subnets" "subnets" {
 	values = [var.default_vpc_id]
   }
 }
+
+data "aws_ssm_parameter" "master_username" {
+  name = "docdb-${var.env}-master_username"
+}
+
+data "aws_ssm_parameter" "master_password" {
+  name = "docdb-${var.env}-master_password"
+}
--- main.tf
+++ main.tf
@@ -45,5 +45,7 @@ module "docdb" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
   engine_version          = each.value["engine_version"]
+  master_username         = data.aws_ssm_parameter.master_username.value
+  master_password         = data.aws_ssm_parameter.master_password.value
 }
 
