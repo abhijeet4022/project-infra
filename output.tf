6a5387b3d1187b7a8cb481bdc27da4d1edecd200
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-29 16:37:42 +0530

    Project App

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-output "vpc" {
-  value = module.vpc
-}
-
-output "vpc_id" {
-  value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-}
-
-output "Abhijeet" {
-  value = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
-}
+# output "vpc" {
+#   value = module.vpc
+# }
+#
+# output "vpc_id" {
+#   value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+# }
+#
+# output "Abhijeet" {
+#   value = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
+# }
 
 output "arn" {
-  value = lookup(module.alb, "private", null)
+  value = lookup(module.alb, "internal", null)
 }
