a4061cbd0169b527744fea6108a6e63b5ff2408e
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-30 16:23:51 +0530

    Project App

--- main.tf
+++ main.tf
@@ -61,89 +61,90 @@ module "app" {
   private_alb_ip_address = local.private_alb_ip_address
   public_listener_arn    = local.public_listener_arn
   image_id               = data.aws_ami.ami.id
+  depends_on             = [module.docdb, module.aurora, module.elasticache, module.rabbitmq]
 }
 
 
 # Deploy the DocumentDB.
-# module "docdb" {
-#   source = "git::https://github.com/abhijeet4022/terraform-aws-docdb.git"
-#
-#   for_each                = var.docdb
-#   env                     = var.env
-#   tags                    = var.tags
-#   db_subnets              = local.db_subnets
-#   app_subnets_cidr        = local.app_subnets_cidr
-#   engine_family           = each.value["engine_family"]
-#   vpc_id                  = local.main_vpc_id
-#   backup_retention_period = each.value["backup_retention_period"]
-#   preferred_backup_window = each.value["preferred_backup_window"]
-#   skip_final_snapshot     = each.value["skip_final_snapshot"]
-#   engine_version          = each.value["engine_version"]
-#   master_username         = data.aws_ssm_parameter.docdb_master_username.value
-#   master_password         = data.aws_ssm_parameter.docdb_master_password.value
-#   instance_count          = each.value["instance_count"]
-#   instance_class          = each.value["instance_class"]
-# }
+module "docdb" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-docdb.git"
+
+  for_each                = var.docdb
+  env                     = var.env
+  tags                    = var.tags
+  db_subnets              = local.db_subnets
+  app_subnets_cidr        = local.app_subnets_cidr
+  engine_family           = each.value["engine_family"]
+  vpc_id                  = local.main_vpc_id
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  engine_version          = each.value["engine_version"]
+  master_username         = data.aws_ssm_parameter.docdb_master_username.value
+  master_password         = data.aws_ssm_parameter.docdb_master_password.value
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
+}
 
 # Deploy the Aurora SQL DB.
-# module "aurora" {
-#   source = "git::https://github.com/abhijeet4022/terraform-aws-aurorasql.git"
-#
-#   for_each                = var.aurora
-#   env                     = var.env
-#   tags                    = var.tags
-#   app_subnets_cidr        = local.app_subnets_cidr
-#   vpc_id                  = local.main_vpc_id
-#   db_subnets              = local.db_subnets
-#   rds_type                = each.value["rds_type"]
-#   engine_family           = each.value["engine_family"]
-#   port                    = each.value["port"]
-#   engine                  = each.value["engine"]
-#   engine_version          = each.value["engine_version"]
-#   backup_retention_period = each.value["backup_retention_period"]
-#   preferred_backup_window = each.value["preferred_backup_window"]
-#   database_name           = data.aws_ssm_parameter.rds_database_name.value
-#   master_username         = data.aws_ssm_parameter.rds_master_username.value
-#   master_password         = data.aws_ssm_parameter.rds_master_password.value
-#   skip_final_snapshot     = each.value["skip_final_snapshot"]
-#   instance_count          = each.value["instance_count"]
-#   instance_class          = each.value["instance_class"]
-# }
+module "aurora" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-aurorasql.git"
+
+  for_each                = var.aurora
+  env                     = var.env
+  tags                    = var.tags
+  app_subnets_cidr        = local.app_subnets_cidr
+  vpc_id                  = local.main_vpc_id
+  db_subnets              = local.db_subnets
+  rds_type                = each.value["rds_type"]
+  engine_family           = each.value["engine_family"]
+  port                    = each.value["port"]
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  database_name           = data.aws_ssm_parameter.rds_database_name.value
+  master_username         = data.aws_ssm_parameter.rds_master_username.value
+  master_password         = data.aws_ssm_parameter.rds_master_password.value
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
+}
 
 
 # Deploy Redis Elasticache.
-# module "elasticache" {
-#   source = "git::https://github.com/abhijeet4022/terraform-aws-elasticache.git"
-#
-#   for_each         = var.elasticache
-#   env              = var.env
-#   tags             = var.tags
-#   db_subnets       = local.db_subnets
-#   vpc_id           = local.main_vpc_id
-#   app_subnets_cidr = local.app_subnets_cidr
-#   elasticache_type = each.value["elasticache_type"]
-#   engine_family    = each.value["engine_family"]
-#   port             = each.value["port"]
-#   engine           = each.value["engine"]
-#   node_type        = each.value["node_type"]
-#   num_cache_nodes  = each.value["num_cache_nodes"]
-#   engine_version   = each.value["engine_version"]
-# }
+module "elasticache" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-elasticache.git"
+
+  for_each         = var.elasticache
+  env              = var.env
+  tags             = var.tags
+  db_subnets       = local.db_subnets
+  vpc_id           = local.main_vpc_id
+  app_subnets_cidr = local.app_subnets_cidr
+  elasticache_type = each.value["elasticache_type"]
+  engine_family    = each.value["engine_family"]
+  port             = each.value["port"]
+  engine           = each.value["engine"]
+  node_type        = each.value["node_type"]
+  num_cache_nodes  = each.value["num_cache_nodes"]
+  engine_version   = each.value["engine_version"]
+}
 
 
 # Deploy RabbitMQ
-# module "rabbitmq" {
-#   source = "git::https://github.com/abhijeet4022/terraform-aws-rabbitmq.git"
-#
-#   for_each         = var.rabbitmq
-#   tags             = var.tags
-#   env              = var.env
-#   zone_id          = var.zone_id
-#   vpc_id           = local.main_vpc_id
-#   ssh_subnets_cidr = var.ssh_subnets_cidr
-#   app_subnets_cidr = local.app_subnets_cidr
-#   db_subnets       = local.db_subnets
-#   ami_id           = data.aws_ami.ami.id
-#   instance_type    = each.value["instance_type"]
-# }
+module "rabbitmq" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-rabbitmq.git"
+
+  for_each         = var.rabbitmq
+  tags             = var.tags
+  env              = var.env
+  zone_id          = var.zone_id
+  vpc_id           = local.main_vpc_id
+  ssh_subnets_cidr = var.ssh_subnets_cidr
+  app_subnets_cidr = local.app_subnets_cidr
+  db_subnets       = local.db_subnets
+  ami_id           = data.aws_ami.ami.id
+  instance_type    = each.value["instance_type"]
+}
 
