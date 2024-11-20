c2c941577686eb3ba02445db7034ddff45e865b2
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-20 16:08:17 +0530

    Encryption

--- main.tf
+++ main.tf
@@ -64,90 +64,92 @@ module "app" {
   private_alb_ip_address = local.private_alb_ip_address
   public_listener_arn    = local.public_listener_arn
 
-  image_id = data.aws_ami.ami.id
-  #depends_on = [module.alb, module.docdb, module.aurora, module.elasticache, module.rabbitmq]
+  image_id   = data.aws_ami.ami.id
+  depends_on = [module.alb, module.docdb, module.aurora, module.elasticache, module.rabbitmq]
 }
 
 
-# # Deploy the DocumentDB.
-# module "docdb" {
-#  source = "git::https://github.com/abhijeet4022/terraform-aws-docdb.git"
-#
-#  env  = var.env
-#  tags = var.tags
-#
-#  for_each = var.docdb
-#
-#  engine_family           = each.value["engine_family"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot     = each.value["skip_final_snapshot"]
-#  engine_version          = each.value["engine_version"]
-#  instance_count          = each.value["instance_count"]
-#  instance_class          = each.value["instance_class"]
-#
-#  db_subnets       = local.db_subnets
-#  app_subnets_cidr = local.app_subnets_cidr
-#  vpc_id           = local.main_vpc_id
-#
-#  master_username = data.aws_ssm_parameter.docdb_master_username.value
-#  master_password = data.aws_ssm_parameter.docdb_master_password.value
-#
-# }
-#
-# # Deploy the Aurora SQL DB.
-# module "aurora" {
-#  source = "git::https://github.com/abhijeet4022/terraform-aws-aurorasql.git"
-#
-#  env  = var.env
-#  tags = var.tags
-#
-#  for_each = var.aurora
-#
-#  rds_type                = each.value["rds_type"]
-#  engine_family           = each.value["engine_family"]
-#  port                    = each.value["port"]
-#  engine                  = each.value["engine"]
-#  engine_version          = each.value["engine_version"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot     = each.value["skip_final_snapshot"]
-#  instance_count          = each.value["instance_count"]
-#  instance_class          = each.value["instance_class"]
-#
-#  app_subnets_cidr = local.app_subnets_cidr
-#  vpc_id           = local.main_vpc_id
-#  db_subnets       = local.db_subnets
-#
-#  database_name   = data.aws_ssm_parameter.rds_database_name.value
-#  master_username = data.aws_ssm_parameter.rds_master_username.value
-#  master_password = data.aws_ssm_parameter.rds_master_password.value
-#
-# }
-#
-#
-# # Deploy Redis Elasticache.
-# module "elasticache" {
-#  source = "git::https://github.com/abhijeet4022/terraform-aws-elasticache.git"
-#
-#  env  = var.env
-#  tags = var.tags
-#
-#  for_each         = var.elasticache
-#  elasticache_type = each.value["elasticache_type"]
-#  engine_family    = each.value["engine_family"]
-#  port             = each.value["port"]
-#  engine           = each.value["engine"]
-#  node_type        = each.value["node_type"]
-#  num_cache_nodes  = each.value["num_cache_nodes"]
-#  engine_version   = each.value["engine_version"]
-#
-#  db_subnets       = local.db_subnets
-#  vpc_id           = local.main_vpc_id
-#  app_subnets_cidr = local.app_subnets_cidr
-# }
-#
-#
+# Deploy the DocumentDB.
+module "docdb" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-docdb.git"
+
+  env        = var.env
+  tags       = var.tags
+  kms_key_id = var.kms_key_id
+
+  for_each = var.docdb
+
+  engine_family           = each.value["engine_family"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  engine_version          = each.value["engine_version"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
+
+  db_subnets       = local.db_subnets
+  app_subnets_cidr = local.app_subnets_cidr
+  vpc_id           = local.main_vpc_id
+
+  master_username = data.aws_ssm_parameter.docdb_master_username.value
+  master_password = data.aws_ssm_parameter.docdb_master_password.value
+
+}
+
+# Deploy the Aurora SQL DB.
+module "aurora" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-aurorasql.git"
+
+  env        = var.env
+  tags       = var.tags
+  kms_key_id = var.kms_key_id
+
+  for_each = var.aurora
+
+  rds_type                = each.value["rds_type"]
+  engine_family           = each.value["engine_family"]
+  port                    = each.value["port"]
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
+
+  app_subnets_cidr = local.app_subnets_cidr
+  vpc_id           = local.main_vpc_id
+  db_subnets       = local.db_subnets
+
+  database_name   = data.aws_ssm_parameter.rds_database_name.value
+  master_username = data.aws_ssm_parameter.rds_master_username.value
+  master_password = data.aws_ssm_parameter.rds_master_password.value
+
+}
+
+
+# Deploy Redis Elasticache.
+module "elasticache" {
+  source = "git::https://github.com/abhijeet4022/terraform-aws-elasticache.git"
+
+  env  = var.env
+  tags = var.tags
+
+  for_each         = var.elasticache
+  elasticache_type = each.value["elasticache_type"]
+  engine_family    = each.value["engine_family"]
+  port             = each.value["port"]
+  engine           = each.value["engine"]
+  node_type        = each.value["node_type"]
+  num_cache_nodes  = each.value["num_cache_nodes"]
+  engine_version   = each.value["engine_version"]
+
+  db_subnets       = local.db_subnets
+  vpc_id           = local.main_vpc_id
+  app_subnets_cidr = local.app_subnets_cidr
+}
+
+
 # Deploy RabbitMQ
 module "rabbitmq" {
   source = "git::https://github.com/abhijeet4022/terraform-aws-rabbitmq.git"
