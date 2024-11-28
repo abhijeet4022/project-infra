12c5bf7eb5dfe48d3680cf57366211bfb7e10bce
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-11-28 12:35:33 +0530

    Commenting the module.

--- locals.tf
+++ locals.tf
@@ -17,11 +17,11 @@ locals {
   ]
 
   # LB Details
-  private_listener_arn = lookup(lookup(lookup(module.alb, "internal", null), "listener", null), "arn", null)
-  public_listener_arn  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
-
-  private_alb_dns_name   = lookup(lookup(lookup(module.alb, "internal", null), "alb", null), "dns_name", null)
-  public_alb_dns_name    = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
+  # private_listener_arn = lookup(lookup(lookup(module.alb, "internal", null), "listener", null), "arn", null)
+  # public_listener_arn  = lookup(lookup(lookup(module.alb, "public", null), "listener", null), "arn", null)
+  #
+  # private_alb_dns_name   = lookup(lookup(lookup(module.alb, "internal", null), "alb", null), "dns_name", null)
+  # public_alb_dns_name    = lookup(lookup(lookup(module.alb, "public", null), "alb", null), "dns_name", null)
   private_alb_ip_address = data.dns_a_record_set.main.addrs
 
 }
