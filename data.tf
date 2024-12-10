22f58a0fc44a13bd52ffc1666a4f490bb417dd2d
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-12-10 18:30:00 +0530

    Applied the changes as per new AWS AC.

--- data.tf
+++ data.tf
@@ -20,29 +20,29 @@ data "aws_ami" "ami" {
   owners      = ["940482433230"]
 }
 
-# # Find the parameter store values for DocDB
-# data "aws_ssm_parameter" "docdb_master_username" {
-#   name = "docdb.${var.env}.master_username"
-# }
-#
-# data "aws_ssm_parameter" "docdb_master_password" {
-#   name = "docdb.${var.env}.master_password"
-# }
-#
-# # Find the parameters store value for Aurora MySQL
-# data "aws_ssm_parameter" "rds_master_username" {
-#   name = "rds.${var.env}.master_username"
-# }
-#
-# data "aws_ssm_parameter" "rds_database_name" {
-#   name = "rds.${var.env}.database_name"
-# }
-#
-# data "aws_ssm_parameter" "rds_master_password" {
-#   name = "rds.${var.env}.master_password"
-# }
+# Find the parameter store values for DocDB
+data "aws_ssm_parameter" "docdb_master_username" {
+  name = "docdb.${var.env}.master_username"
+}
 
-# # Find the IP address for Private LB.
+data "aws_ssm_parameter" "docdb_master_password" {
+  name = "docdb.${var.env}.master_password"
+}
+
+# Find the parameters store value for Aurora MySQL
+data "aws_ssm_parameter" "rds_master_username" {
+  name = "rds.${var.env}.master_username"
+}
+
+data "aws_ssm_parameter" "rds_database_name" {
+  name = "rds.${var.env}.database_name"
+}
+
+data "aws_ssm_parameter" "rds_master_password" {
+  name = "rds.${var.env}.master_password"
+}
+
+# Find the IP address for Private LB.
 # data "dns_a_record_set" "main" {
 #   host = local.private_alb_dns_name
 # }
